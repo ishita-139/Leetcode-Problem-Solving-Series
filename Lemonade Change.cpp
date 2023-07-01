5b9931dcadb7169e0fc04d3cbc191528d7be025b
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-07-01 20:21:23 +0530

    Create Lemonade Change.cpp

---
+++ Lemonade Change.cpp
@@ -0,0 +1,36 @@
+bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(int i : bills)
+        {
+            if(i == 5)
+            {
+                five++;
+            }
+            else if(i == 10)
+            {
+                if(five)
+                {
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(ten>=1 && five>=1)
+                {
+                    ten--;
+                    five--;
+                }
+                else if(five>=3)
+                {
+                    five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }

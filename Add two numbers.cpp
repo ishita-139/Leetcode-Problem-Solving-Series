457dbfc51dee88379c40ab1016508a49d0a33091
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-07-11 20:09:50 +0530

    Update Add two numbers.cpp

--- Add two numbers.cpp
+++ Add two numbers.cpp
@@ -1,1 +1,22 @@
-
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* newList = new ListNode();
+        ListNode* ptr = newList;
+        int carry = 0;
+        while(l1 || l2 || carry){
+            int sum=0;
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2 = l2->next;
+            }
+            sum+=carry;
+            carry = sum/10;
+            ListNode* node = new ListNode(sum%10);
+            ptr->next = node;
+            ptr=ptr->next;
+        }
+        return newList->next;
+    }

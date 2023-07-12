54495586607b097cfba4ac8564576063c51188ea
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-07-12 16:56:12 +0530

    Create Find smallest letter greater than target.cpp

---
+++ Find smallest letter greater than target.cpp
@@ -0,0 +1,8 @@
+char nextGreatestLetter(vector<char>& letters, char target) {
+        for (char letter : letters) {
+            if (letter > target) {
+                return letter;
+            }
+        }
+        return letters[0];
+    }

54fbcba0883cea24f1b11a7d46fc7f3ab382e998
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2023-07-13 18:58:23 +0530

    Create Arranging Coins.cpp

---
+++ Arranging Coins.cpp
@@ -0,0 +1,13 @@
+int arrangeCoins(int n) {
+        int ans=0,i=1;
+        while(n>0)
+        {
+            n=n-i;
+            if (n>=0)
+            {
+                ans++;
+            }
+            i++;
+        }
+        return ans;
+    }
